3b5cee94256f8b9e1728a6ceafd8f18720ffc4e4
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-08 14:47:08 -0600

    Define an instance method
    Make use of the splat operator in order to accept multiple arguments; implement an example of monkey patching

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+
+  def keys_of(*arguments)
+    arr_to_return = []
+    arguments.collect do |arg|
+      self.each do |key, value|
+        if value ==  arg
+          arr_to_return << key
+        end
+      end
+    end
+    arr_to_return
   end
+
 end
